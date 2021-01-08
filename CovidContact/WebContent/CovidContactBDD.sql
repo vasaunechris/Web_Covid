1390a58426d4c48f1b773d4fa61309b14e068dd0
vasaunechris <43403741+vasaunechris@users.noreply.github.com>
2021-01-08 20:24:52 +0100

    Update CovidContactBDD.sql

--- CovidContact/WebContent/CovidContactBDD.sql
+++ CovidContact/WebContent/CovidContactBDD.sql
@@ -1,5 +1,5 @@
 -- ------------------------------------------------------------------------------
--- - Reconstruction de la base de données                                     ---
+-- - Reconstruction de la base de donnÃ©es                                     ---
 -- ------------------------------------------------------------------------------
 DROP DATABASE IF EXISTS CovidContact_BDD;
 CREATE DATABASE CovidContact_BDD;
@@ -43,7 +43,7 @@ CREATE TABLE Admins (
 INSERT INTO Admins VALUES ( 1, "-RWX------" );
 
 -- -----------------------------------------------------------------------------
--- - Construction de la tables des activités                        ---
+-- - Construction de la tables des activitÃ©s                        ---
 -- -----------------------------------------------------------------------------
 CREATE TABLE Activite (
 	Id					int(4)		PRIMARY KEY AUTO_INCREMENT,
@@ -74,54 +74,3 @@ INSERT INTO Lieux (Denomination, Adresse) VALUES ("nom 1", "6 rue du four");
 INSERT INTO Lieux (Denomination, Adresse) VALUES ("nom 2", "23 route du puit");
 INSERT INTO Lieux (Denomination, Adresse) VALUES ("nom 3", "12 rue de la gare");
 
--- -----------------------------------------------------------------------------
--- - Construction de la tables des articles en vente                         ---
--- -----------------------------------------------------------------------------
-CREATE TABLE T_Articles (
-	IdArticle			int(4)		PRIMARY KEY AUTO_INCREMENT,
-	Description			varchar(30)	NOT NULL,
-	Brand				varchar(30)	NOT NULL,
-	UnitaryPrice		float(8)	NOT NULL
-) ENGINE = InnoDB;
-
-INSERT INTO T_Articles ( Description, Brand, UnitaryPrice ) VALUES ( 'Souris',					'Logitoch',				65 );
-INSERT INTO T_Articles ( Description, Brand, UnitaryPrice ) VALUES ( 'Clavier',					'Microhard',			49.5 );
-INSERT INTO T_Articles ( Description, Brand, UnitaryPrice ) VALUES ( 'Systeme d''exploitation',	'Fenetres Vistouille',	150 );
-INSERT INTO T_Articles ( Description, Brand, UnitaryPrice ) VALUES ( 'Tapis souris', 			'Chapeau Bleu',			5 );
-INSERT INTO T_Articles ( Description, Brand, UnitaryPrice ) VALUES ( 'Cle USB 8 To', 			'Syno',					8 );
-INSERT INTO T_Articles ( Description, Brand, UnitaryPrice ) VALUES ( 'Laptop', 					'PH',					1199 );
-INSERT INTO T_Articles ( Description, Brand, UnitaryPrice ) VALUES ( 'CD x 500', 				'CETME',				250 );
-INSERT INTO T_Articles ( Description, Brand, UnitaryPrice ) VALUES ( 'DVD-R x 100', 			'CETME',				99 );
-INSERT INTO T_Articles ( Description, Brand, UnitaryPrice ) VALUES ( 'DVD+R x 100', 			'CETME',				105 );
-INSERT INTO T_Articles ( Description, Brand, UnitaryPrice ) VALUES ( 'Batterie Laptop', 		'PH',					80 );
-INSERT INTO T_Articles ( Description, Brand, UnitaryPrice ) VALUES ( 'Casque Audio', 			'Syno',					105 );
-INSERT INTO T_Articles ( Description, Brand, UnitaryPrice ) VALUES ( 'WebCam', 					'Logitoch',				755 );
-
-SELECT * FROM T_Articles;
-
--- -----------------------------------------------------------------------------
--- - Construction de la tables des commandes                                 ---
--- -----------------------------------------------------------------------------
-CREATE TABLE T_Commands (
-	IdCommand		int(4)		PRIMARY KEY AUTO_INCREMENT,
-	IdUser			int(4)		NOT NULL REFERENCES T_Users(IdUser),
-    CommandDate     datetime    NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE = InnoDB;
-
-CREATE TABLE T_CommandLines (
-	IdCommandLine	int(4)		PRIMARY KEY AUTO_INCREMENT,
-	IdCommand		int(4)		NOT NULL REFERENCES T_Commands(IdCommand),
-	IdArticle		int(4)		NOT NULL REFERENCES T_Articles(IdCommand),
-	Quantity		int(4) 		NOT NULL
-) ENGINE = InnoDB;
-
--- Une première commande
-INSERT INTO T_Commands (IdUser, CommandDate) VALUES ( 2, now() ); 
-INSERT INTO T_CommandLines (IdCommand, IdArticle, Quantity) VALUES (1, 1, 5); 
-INSERT INTO T_CommandLines (IdCommand, IdArticle, Quantity) VALUES (1, 3, 3);
-
--- Une seconde commande, pour un admin
-INSERT INTO T_Commands (IdUser, CommandDate) VALUES ( 1, now() ); 
-INSERT INTO T_CommandLines (IdCommand, IdArticle, Quantity) VALUES (2, 2, 4); 
-INSERT INTO T_CommandLines (IdCommand, IdArticle, Quantity) VALUES (2, 3, 1);
-INSERT INTO T_CommandLines (IdCommand, IdArticle, Quantity) VALUES (2, 4, 1);
